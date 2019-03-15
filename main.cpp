900f38af6831b88b25543c54551cb9a6a8355a20
Peter Hammar <peca_bis@hotmail.com>
2019-03-15 16:36:47 +0100

    kommentarer

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 int main()
 {
   // select object
+  Game game;
+  char ans;
+  do {
+    game.selectGameObject("knife");
+    std::cout << "x to exit" << std::endl;
+    std::cin >> ans;
+  } while(ans != 'x');
 
   // present interaction options for object
 
@@ -12,13 +19,6 @@ int main()
   // select interaction
 
   // print outcome
-  Game game;
-  char ans;
-  do {
-    game.selectGameObject("knife");
-    std::cout << "x to exit" << std::endl;
-    std::cin >> ans;
-  } while(ans != 'x');
 }
 
 std::string flag[4][8]

7b0596452b44ac99a30109f30d44c5d7facf05f0
Peter Hammar <peca_bis@hotmail.com>
2019-03-15 16:34:57 +0100

    kommentarer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 int main()
 {
+  // select object
+
+  // present interaction options for object
+
+  // select interaction
+
+  // present interaction specific options
+
+  // select interaction
+
+  // print outcome
   Game game;
   char ans;
   do {

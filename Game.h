f5f452f148111a97b73e79fab7ba4402cbd74e37
Mimslade <Anton.maxen@gmail.com>
2019-03-15 17:01:44 +0100

    visual

--- Game.h
+++ Game.h
@@ -1,12 +1,16 @@
 #ifndef GAME_H
 #define GAME_H
+#include <iostream>
+#include <string>
 
 class Game
 {
     private:
         int currentObject;
-        std::string flag = "00000000111111112222222233333333";
+        std::string flag = "10101010110011001110001111110000";
         std::string gameObjects[4] = {"knife", "painting", "gun", "hammer"};
+        std::string interactionTypes[8] = {"look", "open", "move", "turnon",
+                                           "turnoff", "taste", "pickup", "drop"};
     public:
         Game() {
 
@@ -20,7 +24,11 @@ class Game
         }
         void selectInteraction() {
             for(int i = currentObject * 8; i < (currentObject + 1) * 8; i++) {
-              std::cout << "Flag: " << flag[i] << std::endl;
+              std::cout << interactionTypes[i - currentObject * 8] << ": ";
+              if(flag[i] == '1')
+                std::cout << "true" << std::endl;
+              else
+                std::cout << "false" << std::endl;
             }
         }
 };
